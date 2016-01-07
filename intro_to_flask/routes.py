92397a05eb2b216cebb7d850657eddc59ff59240
unknown <Evans@Level3-001.ml.local>
2016-01-07 15:17:49 +0000

    first commit

---
+++ intro_to_flask/routes.py
@@ -0,0 +1,36 @@
+from intro_to_flask import app
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+from flask.ext.mail import Message, Mail
+
+mail = Mail()
+ 
+@app.route('/')
+def home():
+  return render_template('home.html')
+ 
+@app.route('/about')
+def about():
+  return render_template('about.html')
+  
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm()
+ 
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+      msg.body = """
+      From: %s &lt;%s&gt;
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+ 
+      return render_template('contact.html', success=True)
+ 
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
+ 
